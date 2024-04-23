0158d4910da87b2bb4629ba3bc7cb9ef04a8481a
roxelioviewnext <roxelio.vellor@viewnext.com>
2024-04-23 12:34:52 +0200

    crear constructores, setters y getter

--- Proyecto/Biblioteca/src/com/biblioteca/clases/Documento.java
+++ Proyecto/Biblioteca/src/com/biblioteca/clases/Documento.java
@@ -8,8 +8,51 @@ public abstract class Documento implements DocumentoInterfaz {
 	private boolean isDisponible;
 	private int totalEjemplares;
 	
+	
+	
+	public Documento(int idDocumento, String titulo, boolean isDisponible, int totalEjemplares) {
+		super();
+		this.idDocumento = idDocumento;
+		this.titulo = titulo;
+		this.isDisponible = isDisponible;
+		this.totalEjemplares = totalEjemplares;
+	}
+
 	@Override
 	public abstract int calcularTotalEjemplares() ;
+	
+	
+	public int getIdDocumento() {
+		return idDocumento;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public boolean isDisponible() {
+		return isDisponible;
+	}
+
+	public int getTotalEjemplares() {
+		return totalEjemplares;
+	}
+
+	public void setIdDocumento(int idDocumento) {
+		this.idDocumento = idDocumento;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public void setDisponible(boolean isDisponible) {
+		this.isDisponible = isDisponible;
+	}
+
+	public void setTotalEjemplares(int totalEjemplares) {
+		this.totalEjemplares = totalEjemplares;
+	}
 	
 	
 	
--- Proyecto/Biblioteca/src/com/biblioteca/clases/documentos/Libro.java
+++ Proyecto/Biblioteca/src/com/biblioteca/clases/documentos/Libro.java
@@ -4,14 +4,37 @@ import java.time.LocalDate;
 
 import com.biblioteca.clases.Documento;
 
-public final class Libro extends Documento{
+public final class Libro extends Documento {
 	private LocalDate anhoPublicacion;
 	private String autor;
+
+	public Libro(int idDocumento, String titulo, boolean isDisponible, int totalEjemplares, LocalDate anhoPublicacion,
+			String autor) {
+		super(idDocumento, titulo, isDisponible, totalEjemplares);
+		this.anhoPublicacion = anhoPublicacion;
+		this.autor = autor;
+	}
 
 	@Override
 	public int calcularTotalEjemplares() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-	
+
+	public LocalDate getAnhoPublicacion() {
+		return anhoPublicacion;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAnhoPublicacion(LocalDate anhoPublicacion) {
+		this.anhoPublicacion = anhoPublicacion;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
 }
