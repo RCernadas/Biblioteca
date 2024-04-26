1fbc5f41ce1813c8e366c31f372de6b7aae028f8
roxelioviewnext <roxelio.vellor@viewnext.com>
2024-04-26 14:57:37 +0200

    cambios viernes tarde actualizacion

--- Proyecto/Biblioteca/src/com/biblioteca/bibliotecario/Bibliotecario.java
+++ Proyecto/Biblioteca/src/com/biblioteca/bibliotecario/Bibliotecario.java
@@ -22,7 +22,7 @@ public class Bibliotecario implements BibliotecarioInterfaz {
 	public void prestarDocumento(String dni) {
 		if (documentoActual != null) {
 			Usuario usuario = Consultas.PrestarDocumento(dni, documentoActual);
-			System.out.println("Se ha prestado el documento actual: "+getDocumentoActual() +" a: -> " + usuario.getDni() );
+			System.out.println("Se ha devuelto el documento actual: "+getDocumentoActual() +" a: -> " + usuario.getDni() );
 			documentoActual = null;
 		}else System.out.println("No se ha podido prestar el documento porque no has seleccionado ningun documento");
 		
@@ -30,7 +30,11 @@ public class Bibliotecario implements BibliotecarioInterfaz {
 
 	@Override
 	public void devolverDocumento(String dni) {
-		// TODO Auto-generated method stub
+		if (documentoActual != null) {
+			Usuario usuario = Consultas.DevolverDocumentoActual(documentoActual , dni);
+			System.out.println("Se ha prestado el documento actual: "+getDocumentoActual() +" a: -> " + usuario.getDni() );
+			documentoActual = null;
+		}else System.out.println("No se ha podido devolver el documento porque no has seleccionado ningun documento");
 
 	}
 
