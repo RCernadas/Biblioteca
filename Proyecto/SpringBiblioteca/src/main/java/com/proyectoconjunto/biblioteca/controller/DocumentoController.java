8acb8a2895d4a5d4b8d156dc014fe39b524e27fd
roxelioviewnext <roxelio.vellor@viewnext.com>
2024-04-29 16:33:37 +0200

    la mierda esta del spring

--- Proyecto/SpringBiblioteca/src/main/java/com/proyectoconjunto/biblioteca/controller/DocumentoController.java
+++ Proyecto/SpringBiblioteca/src/main/java/com/proyectoconjunto/biblioteca/controller/DocumentoController.java
@@ -1,5 +1,22 @@
 package com.proyectoconjunto.biblioteca.controller;
 
-public class DocumentoController {
+import java.lang.ProcessBuilder.Redirect;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.view.AbstractView;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+public class DocumentoController {
+	
+	@GetMapping(value = "/")
+	public RedirectView pruebaSelene(Model model){
+	     return new RedirectView("welcome");
+	}
 }
--- Proyecto/SpringBiblioteca/src/main/webapp/WEB-INF/views/welcome.jsp
+++ Proyecto/SpringBiblioteca/src/main/webapp/WEB-INF/views/welcome.jsp
@@ -1,11 +1,3 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Welcome</title>
-</head>
-<body>
-
-</body>
-</html>
+<%@ include file="common/header.jsp" %>
+		HOLA MUNDO
+<%@ include file="common/footer.jsp" %>
